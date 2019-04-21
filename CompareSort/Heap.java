a12bd503f1d8e9a95487a8af30c65c0eef342cf0
Vraj Mistry <vrajparesh22@gmail.com>
2019-04-22 00:59:13 +0530

    Add files via upload
    Added Heap Sort

---
+++ CompareSort/Heap.java
@@ -0,0 +1,36 @@
+package CompareSort;
+public class Heap
+{
+    public long sort(int arr[]) 
+    {   long start=Timer.start();
+        int n = arr.length; 
+        for (int i = n / 2 - 1; i >= 0; i--) 
+            heapify(arr, n, i); 
+        for (int i=n-1; i>=0; i--) 
+        { 
+            int temp = arr[0]; 
+            arr[0] = arr[i]; 
+            arr[i] = temp; 
+            heapify(arr, i, 0); 
+        } 
+        long end=Timer.end();
+        return (end-start);
+    } 
+    void heapify(int arr[], int n, int i) 
+    { 
+        int largest = i;  // Initialize largest as root 
+        int l = 2*i + 1;  // left = 2*i + 1 
+        int r = 2*i + 2;  // right = 2*i + 2 
+        if (l < n && arr[l] > arr[largest]) 
+            largest = l; 
+        if (r < n && arr[r] > arr[largest]) 
+            largest = r; 
+        if (largest != i) 
+        { 
+            int swap = arr[i]; 
+            arr[i] = arr[largest]; 
+            arr[largest] = swap; 
+            heapify(arr, n, largest); 
+        } 
+    } 
+}
